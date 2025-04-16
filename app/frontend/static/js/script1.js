03c1b674bd0c6b532462182dce933d26f8f549ce
Ashi228 <147255375+Ashi228@users.noreply.github.com>
2025-04-16 16:40:30 +0300

    Update script1.js

--- app/frontend/static/js/script1.js
+++ app/frontend/static/js/script1.js
@@ -17,4 +17,17 @@ document.addEventListener('DOMContentLoaded', () => {
         // Переход на страницу форума
         window.location.href = 'forum'; // Замените на нужный URL
     });
+
+    const tabEditor = document.getElementById('tab-editor');
+    const tabOutput = document.getElementById('tab-output');
+
+    tabOutput.addEventListener('click', () => {
+        tabOutput.classList.add('active');
+        tabEditor.classList.remove('active');
+    });
+
+    tabEditor.addEventListener('click', () => {
+        tabEditor.classList.add('active');
+        tabOutput.classList.remove('active');
+    });
 });
